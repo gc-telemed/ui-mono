36e147e49ebb722357a0c8eb1c6933220a8746d9
Risav Karna <git@risav.dev>
2022-11-18 17:13:37 +0545

    feat(doq-react): add react project for patient-facing doq ui to complement doq-karan

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   content: [
     "./apps/backoffice/src/**/*.{html,ts}",
     "./libs/ui/src/lib/**/*.{html,ts}",
-    "./apps/karan/src/**/!(*.stories|*.spec).{ts,html}"
+    "./apps/karan/src/**/!(*.stories|*.spec).{ts,html}",
+    "./apps/doq/src/**/*.{js,jsx,ts,tsx,html}"
   ],
   theme: {
     extend: {},
