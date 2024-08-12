a67f8b68ed7005123760d41035b2bdd1923151f0
Risav Karna <git@risav.dev>
2024-08-12 23:57:17 +0545

    build(standalone): convert backoffice to standalone style

--- apps/backoffice/src/app/pages/editor/editor/editor.component.ts
+++ apps/backoffice/src/app/pages/editor/editor/editor.component.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { TabularModule } from '../../../features/tabular/tabular.module';
 
 @Component({
     selector: 'gita-editor',
     templateUrl: './editor.component.html',
     styleUrls: ['./editor.component.scss'],
     standalone: true,
-    imports: [AsyncPipe],
+    imports: [AsyncPipe, TabularModule],
 })
 export class EditorComponent implements OnInit {
 
@@ -23,7 +24,7 @@ export class EditorComponent implements OnInit {
         'Content-Type': 'application/json',
       }
     }).pipe(
-      map(res => JSON.stringify(res))
+      map(res => JSON.stringify(res, null, 4))
     );
 
     this.$resp.subscribe(res => console.log("res", res));
